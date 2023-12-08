2ec3548f967706d6f5b19a6c0374700bdd2ae5c5
alexander-ponce <alex@poncealex.com>
2023-12-08 11:23:40 -0800

    first commit

---
+++ frida.py
@@ -0,0 +1,6 @@
+paintings = ['The Two Fridas', 'My Dress Hangs Here, Tree of Hope', 'Self Portrait With Monkeys']
+
+dates = [1939, 1933, 1946, 1940]
+
+paintings =  list(zip(dates, paintings))
+print(paintings)
